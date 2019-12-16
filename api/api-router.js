7651a055c20154c7c8c2a255702c63a2ccf27dd7
Rasha Moumneh <rasha@fastmail.com>
2019-12-16 18:16:12 -0500

    Authorization middleware

--- api/api-router.js
+++ api/api-router.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 
 const Users = require("./auth-model");
 
-router.get("/users", async (req, res) => {
+router.get("/users", restricted, async (req, res) => {
   const allUsers = await Users.getUsers();
   try {
     res.status(200).json(allUsers);
@@ -26,19 +26,35 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   let { username, password } = req.body;
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: "You shall not pass!" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+  const user = await Users.findBy({ username }).first();
+  try {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({ message: `Welcome ${user.username}!` });
+    } else {
+      res.status(401).json({ message: "You shall not pass!" });
+    }
+  } catch (error) {
+    res.status(500).json("Error!");
+  }
 });
+
+// Authorization middleware
+async function restricted(req, res, next) {
+  const { username, password } = req.headers;
+  const user = await Users.findBy({ username }).first();
+  if (username && password) {
+    try {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Unexpected error" });
+    }
+  } else res.status(400).json({ message: "No credentials provided" });
+}
+
 module.exports = router;
